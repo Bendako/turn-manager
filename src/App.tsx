5393152912be7ea2c34f6c4d50fe538f45bb6b71
Bendako <62848655+Bendako@users.noreply.github.com>
2024-06-23 06:49:36 +0300

    screens fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Card from './components/Card';
 
 const App: React.FC = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Card name="בן"/>
-      <Card name="דוד"/>
-      <Card name="שלומי"/>
-      <Card name="נווה"/>
-      <Card name="מתן"/>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="flex flex-wrap justify-center">
+        <Card name="בן" />
+        <Card name="דוד" />
+        <Card name="שלומי" />
+        <Card name="נווה" />
+        <Card name="מתן" />
+      </div>
     </div>
   );
 };
